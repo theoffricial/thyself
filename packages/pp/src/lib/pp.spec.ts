4f4be2fd978769cac53e360274ccaa37758e8cc3
Ofri Peretz <theoffricial@gmail.com>
2024-02-10 17:08:41 +0200

    deps_upgrade: upgrade nx to 18 and remove redundant projects

--- packages/pp/src/lib/pp.spec.ts
+++
@@ -1,7 +0,0 @@
-import { pp } from './pp';
-
-describe('pp', () => {
-  it('should work', () => {
-    expect(pp()).toEqual('pp');
-  });
-});
--- packages/pp/src/lib/pp.ts
+++
@@ -1,3 +0,0 @@
-export function pp(): string {
-  return 'pp';
-}
